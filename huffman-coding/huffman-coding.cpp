f1ccff1355ed2ac59a7628deab07c44c71bfff81
nvh202sua <34486659+nvh202sua@users.noreply.github.com>
2021-11-28 19:29:26 +0700

    3.1

--- huffman-coding/huffman-coding.cpp
+++ huffman-coding/huffman-coding.cpp
@@ -1,20 +1,5 @@
-#include <iostream>
-#include "header/TreeNode.h"
 #include "header/Menu.h"
 
-using namespace std;
-
-void inOrder(TreeNode *node) {
-    if (node == nullptr)
-        return;
-    cout << node->getEle() << ": " << node->getFreq() << " ";
-    inOrder(node->getLeft());
-    inOrder(node->getRight());
-}
-
 int main() {
-    string msg = "ABABBCBBDEEEABABBAEEDDCCABBBCDEEDCBCCCCDBBBCAAA";
-    string res = "00110011110111111001011011010011001111001011011001000101001111110110010110110001110101010110011111101000000";
-
     Menu::primary();
 }
