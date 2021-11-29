b27c0657aaf1ed7d4c5de4080832e6eec28c5745
nvh202sua <34486659+nvh202sua@users.noreply.github.com>
2021-11-29 12:05:11 +0700

    up trenode

--- huffman-coding/TreeNode.cpp
+++ huffman-coding/TreeNode.cpp
@@ -1,50 +1,41 @@
 #include "header/TreeNode.h"
 
-TreeNode::TreeNode()
-{
+TreeNode::TreeNode() {
     left = right = nullptr;
 }
 
-TreeNode::TreeNode(char ele, uint fre)
-{
+TreeNode::TreeNode(char ele, uint fre) {
     this->element = ele;
     this->freq = fre;
     left = right = nullptr;
 }
 
-TreeNode::~TreeNode()
-{
+TreeNode::~TreeNode() {
     delete left;
     delete right;
 }
 
-char TreeNode::getEle()
-{
+char TreeNode::getEle() {
     return element;
 }
 
-uint TreeNode::getFreq()
-{
+uint TreeNode::getFreq() {
     return freq;
 }
 
-void TreeNode::setLeft(TreeNode *left)
-{
+void TreeNode::setLeft(TreeNode *left) {
     this->left = left;
 }
 
-TreeNode* TreeNode::getLeft()
-{
+TreeNode* TreeNode::getLeft() {
     return left;
 }
 
-void TreeNode::setRight(TreeNode *right)
-{
+void TreeNode::setRight(TreeNode *right) {
     this->right = right;
 }
 
-TreeNode* TreeNode::getRight()
-{
+TreeNode* TreeNode::getRight() {
     return right;
 }
 
